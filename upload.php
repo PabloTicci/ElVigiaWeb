d143ecb093620fdb5bc9a2c9a476c448f24d6c34
Pablo Gomez <78189254+PabloTicci@users.noreply.github.com>
2023-10-30 11:41:50 -0600

    Correcion Bootstrap

--- upload.php
+++ upload.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+  <link rel="stylesheet" href="css/upload.css">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Subir Noticias</title>
 
   <!-- Agrega los enlaces a Bootstrap y a tu archivo de estilos CSS personalizado -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/upload.css"> <!-- Crea un archivo estilos.css en el mismo directorio -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap">
+   <!-- Crea un archivo estilos.css en el mismo directorio -->
 
   <!-- Agrega scripts de Bootstrap y jQuery (necesarios para algunos componentes de Bootstrap) -->
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/js/bootstrap.min.js"></script>
 </head>
 <body>
   <div class="container mt-5">
@@ -60,5 +60,9 @@
       </div>
     </div>
   </div>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/js/bootstrap.min.js"></script>
+  
 </body>
 </html>
