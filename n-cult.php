66c038bd102143daaf8302d9184b7a8d54fb7340
Pablo Gomez <78189254+PabloTicci@users.noreply.github.com>
2023-11-02 14:36:56 -0600

    Implementaci

--- n-cult.php
+++ n-cult.php
@@ -8,38 +8,50 @@
 
 <head>
     <link href="css/styles.css" rel="stylesheet">
-    <link href="css/noticia.css" rel="stylesheet">
     <meta charset="UTF-8">
-    <meta name="viewerpoint" content="width-device-widht, initial-sacel=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>EL VIGIA HGO</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5119595055377524"
      crossorigin="anonymous"></script>
+
 </head>
 
 <body>
 <header class="header">
-    <nav class="navbar navbar navbar-expand-lg bg-body-tertiary fixed-top">
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
         <div class="container-fluid">
-          <a href="index.php"><img src="resources/img/elvigialogo.png" class="logo-nav"></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link" aria-current="page" href="n-estat.php">NOTICIAS ESTATALES</a>
-              <a class="nav-link" href="n-nacio.php">NOTICIAS NACIONALES</a>
-              <a class="nav-link" href="n-inter.php">NOTICIAS INTERNACIONALES</a>
-              <a class="nav-link" href="n-depo.php">DEPORTES</a>
-              <a class="nav-link" href="n-cult.php">CULTURA/ESPECTACULOS</a>
-              <a class="nav-link" href="n-cald.php">CALDERO MAGICO</a>
-              <span class="indicador" id="indicador"></span>
+            <a href="index.php"><img src="resources/img/elvigialogo.png" class="logo-nav"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <button class="cerrar"><span class="navbar-toggler-icon"></span></button>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="n-estat.php">NOTICIAS ESTATALES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="n-nacio.php">NOTICIAS NACIONALES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="n-inter.php">NOTICIAS INTERNACIONALES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="n-depo.php">DEPORTES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="n-cult.php">CULTURA/ESPECTACULOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="n-cald.php">CALDERO MAGICO</a>
+                    </li>
+                </ul>
             </div>
-          </div>
         </div>
-      </nav>
-</header>>
+    </nav>
+</header>
 
 <div class="container cont-noti">
         <div class="Noticias">
