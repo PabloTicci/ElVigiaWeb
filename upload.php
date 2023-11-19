51029287e8e44f8b8be5427a279a4e5ef1d47c45
Pablo Gomez <78189254+PabloTicci@users.noreply.github.com>
2023-11-19 14:25:50 -0600

    Deshacer correccion

--- upload.php
+++ upload.php
@@ -7,13 +7,6 @@
   <title>Subir Noticias</title>
 
 
-<script>
-  $(document).ready(function(){
-    $('form').submit(function(){
-      $('button').attr('disabled', true);
-    });
-  });
-</script>
 
   <!-- Agrega los enlaces a Bootstrap y a tu archivo de estilos CSS personalizado -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
