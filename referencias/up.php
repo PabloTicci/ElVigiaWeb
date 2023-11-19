ebcebc3b46b9b2de064e27f22092af3dffd5ec91
Pablo Gomez <78189254+PabloTicci@users.noreply.github.com>
2023-11-19 14:22:06 -0600

    correccion error subir noticias

--- referencias/up.php
+++ referencias/up.php
@@ -1,5 +1,8 @@
 <?php
 
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
 $servername = "localhost";
 $username = "u630558413_Pablo";
 $password = "Lobitonan1723";
