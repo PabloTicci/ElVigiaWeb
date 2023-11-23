77fcfb032f61c845ac466b6327ea70d4db187b3a
Pablo Gomez <78189254+PabloTicci@users.noreply.github.com>
2023-11-22 21:50:29 -0600

    Correccion de subida de archivos

--- upload.php
+++ upload.php
@@ -6,14 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Subir Noticias</title>
 
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-  $(document).ready(function(){
-    $('form').submit(function(){
-      $('button').attr('disabled', true);
-    });
-  });
-</script>
 
   <!-- Agrega los enlaces a Bootstrap y a tu archivo de estilos CSS personalizado -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
