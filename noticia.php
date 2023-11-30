a30d2480decfb020847e1a0024d70f0579417804
Pablo Gomez <78189254+PabloTicci@users.noreply.github.com>
2023-11-30 12:59:18 -0600

    Modificaciones para poder subir multimedia

--- noticia.php
+++ noticia.php
@@ -1,9 +1,3 @@
-<?php
-
-    include ("referencias/noticia.php");
-
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -16,82 +10,88 @@
     <title>EL VIGIA HGO</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5119595055377524"
-     crossorigin="anonymous"></script>
-
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5119595055377524" crossorigin="anonymous"></script>
 </head>
 
 <body>
-<header class="header">
-<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-        <div class="container-fluid">
-            <a href="index.php"><img src="resources/img/elvigialogo.png" class="logo-nav"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <button class="cerrar"><span class="navbar-toggler-icon"></span></button>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="n-estat.php">NOTICIAS ESTATALES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="n-nacio.php">NOTICIAS NACIONALES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="n-inter.php">NOTICIAS INTERNACIONALES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="n-depo.php">DEPORTES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="n-cult.php">CULTURA/ESPECTACULOS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="n-cald.php">CALDERO MAGICO</a>
-                    </li>
-                </ul>
+    <header class="header">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+            <div class="container-fluid">
+                <a href="index.php"><img src="resources/img/elvigialogo.png" class="logo-nav"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <button class="cerrar"><span class="navbar-toggler-icon"></span></button>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="n-estat.php">NOTICIAS ESTATALES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="n-nacio.php">NOTICIAS NACIONALES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="n-inter.php">NOTICIAS INTERNACIONALES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="n-depo.php">DEPORTES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="n-cult.php">CULTURA/ESPECTACULOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="n-cald.php">CALDERO MAGICO</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-</header>
+        </nav>
+    </header>
 
-<div class="container mt-4">
+    <div class="container mt-4">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="news-container">
                     <h2 class="text-primary"><?php echo $titulo ?></h2>
                     <p class="lead"><?php echo $introduccion ?></p>
-                    <img src="<?php echo $fotos ?>" alt="Imagen de la noticia" class="img-fluid rounded mb-3">
+
+                    <!-- Muestra todas las fotos asociadas a la noticia -->
+                    <?php foreach ($fotos as $foto): ?>
+                        <img src="<?php echo $foto ?>" alt="Imagen de la noticia" class="img-fluid rounded mb-3">
+                    <?php endforeach; ?>
+
                     <p><?php echo $noticia ?></p>
                     <p>Fecha de Publicación: <?php echo $fecha_publicacion ?></p>
                     <p>Fuente de la Noticia: <a href="#"><?php echo $fuente ?></a></p>
+
+                    <!-- Muestra todos los videos asociados a la noticia -->
+                    <?php foreach ($videos as $video): ?>
+                        <!-- Agrega tu lógica para mostrar videos (puede ser mediante etiquetas <video> o enlaces a plataformas de videos) -->
+                        <p>Video: <?php echo $video ?></p>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </div>
-</div>
+    </div>
 
-<script src="https://cdn.jsdelivr.net/npm/popper.js@2.10.2/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 
 <footer class="footer">
-  <div class="container">
-    <div class="footer-content">
-      <div class="footer-text">
-        <p>&copy; 2023 El Vigia Hidalgo. Todos los derechos reservados.</p>
-      </div>
-      <div class="footer-icons">
-        <a href="https://m.facebook.com/profile.php/?id=100064576866780" target="_blank" class="face"><img src="resources/img/facebook-icon.png" alt="Facebook"></a>
-        <a href="https://x.com/vigiahidalgo?s=21&t=iynC3EZB_UOgI-jgpQvF6A" target="_blank" class="twit"><img src="resources/img/twitter-icon.png" alt="Twitter"></a>
-        <a href="https://www.tiktok.com/@viga.hidalgo?_t=8gypQDaGQ22&_r=1" alt="TikTok" class="tiktok"><img src="resources/img/logotiktok.png" alt="TikTok"></a>
-      </div>
+    <div class="container">
+        <div class="footer-content">
+            <div class="footer-text">
+                <p>&copy; 2023 El Vigia Hidalgo. Todos los derechos reservados.</p>
+            </div>
+            <div class="footer-icons">
+                <a href="https://m.facebook.com/profile.php/?id=100064576866780" target="_blank" class="face"><img src="resources/img/facebook-icon.png" alt="Facebook"></a>
+                <a href="https://x.com/vigiahidalgo?s=21&t=iynC3EZB_UOgI-jgpQvF6A" target="_blank" class="twit"><img src="resources/img/twitter-icon.png" alt="Twitter"></a>
+                <a href="https://www.tiktok.com/@viga.hidalgo?_t=8gypQDaGQ22&_r=1" alt="TikTok" class="tiktok"><img src="resources/img/logotiktok.png" alt="TikTok"></a>
+            </div>
+        </div>
     </div>
-  </div>
 </footer>
-
-
-
 
 </html>
--- referencias/up.php
+++ referencias/up.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $servername = "localhost";
 $username = "u630558413_Pablo";
 $password = "Lobitonan1723";
@@ -13,9 +12,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+
 if (isset($_POST['btnsubir'])) {
     // Check other fields for emptiness
-    if (empty($_POST["titulo"]) || empty($_POST["noticia"]) || empty($_FILES["imagen"]["name"]) || empty($_POST["fuente"]) || empty($_POST["categoria"])) {
+    if (empty($_POST["titulo"]) || empty($_POST["noticia"]) || empty($_FILES["video"]["name"]) || empty($_POST["fuente"]) || empty($_POST["categoria"])) {
         echo "Acceso denegado";
     } else {
         $titulo = $_POST["titulo"];
@@ -24,23 +24,53 @@ if (isset($_POST['btnsubir'])) {
         $fuente = $_POST["fuente"];
         $categoria = $_POST["categoria"];
 
-        // File upload handling
-        $target_dir = "resources/img";  // Set your target directory
-        $target_file = $target_dir . basename($_FILES["imagen"]["name"]);
-        move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file);
+        // File upload handling for images
+        $imagenes = array();
+        foreach ($_FILES["imagen"]["tmp_name"] as $key => $tmp_name) {
+            $target_dir = "resources/img";
+            $target_file = $target_dir . basename($_FILES["imagen"]["name"][$key]);
+            move_uploaded_file($_FILES["imagen"]["tmp_name"][$key], $target_file);
+            $imagenes[] = $target_file;
+        }
 
-        $consulta = "INSERT INTO noticia (titulo, introduccion, noticia, fotos, fecha_publicacion, fuente, etiquetas) values ('$titulo','$introduccion','$noticia','$target_file',curdate(),'$fuente','$categoria')";
-        $resultado = mysqli_query($conn, $consulta);
+        // File upload handling for video
+        $target_dir_video = "resources/videos";
+        $target_file_video = $target_dir_video . basename($_FILES["video"]["name"]);
+        move_uploaded_file($_FILES["video"]["tmp_name"], $target_file_video);
 
-        if ($resultado) {
+        // Insert multimedia records for images
+        foreach ($imagenes as $imagen) {
+            $consulta_multimedia_imagen = "INSERT INTO multimedia (noticia_id, tipo, archivo_path) VALUES (LAST_INSERT_ID(), 'imagen', ?)";
+            $stmt_multimedia_imagen = $conn->prepare($consulta_multimedia_imagen);
+            $stmt_multimedia_imagen->bind_param("s", $imagen);
+            $stmt_multimedia_imagen->execute();
+        }
+
+        // Insert multimedia record for video
+        $consulta_multimedia_video = "INSERT INTO multimedia (noticia_id, tipo, archivo_path) VALUES (LAST_INSERT_ID(), 'video', ?)";
+        $stmt_multimedia_video = $conn->prepare($consulta_multimedia_video);
+        $stmt_multimedia_video->bind_param("s", $target_file_video);
+        $stmt_multimedia_video->execute();
+
+        // Insert news record
+        $consulta_noticia = "INSERT INTO noticia (titulo, introduccion, noticia, fecha_publicacion, fuente, etiquetas) VALUES (?, ?, ?, curdate(), ?, ?)";
+        $stmt_noticia = $conn->prepare($consulta_noticia);
+        $stmt_noticia->bind_param("sssss", $titulo, $introduccion, $noticia, $fuente, $categoria);
+        $stmt_noticia->execute();
+
+        if ($stmt_noticia->affected_rows > 0) {
             ?>
-            <h3 class="ok"> Registro guardado correctamente </h3>
+            <h3 class="ok">Registro guardado correctamente</h3>
             <?php
         } else {
             ?>
-            <h3 class="Bad"> Error </h3>
+            <h3 class="Bad">Error</h3>
             <?php
         }
+
+        $stmt_multimedia_imagen->close();
+        $stmt_multimedia_video->close();
+        $stmt_noticia->close();
     }
 }
 
