69552f0f015ccd9d1f4606dad311b46ed2162224
Pablo Gomez <78189254+PabloTicci@users.noreply.github.com>
2023-11-30 13:26:00 -0600

    correccion error up

--- referencias/up.php
+++ referencias/up.php
@@ -11,49 +11,46 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-if (isset($_POST['btnsubir'])) {
-    // Otras comprobaciones...
+// Desactivar desencadenadores
+mysqli_query($conn, "SET foreign_key_checks = 0;");
 
+// Realizar la operación de inserción
+if (isset($_POST['btnsubir'])) {
+    // ... (tu código de inserción)
+
+    // Por ejemplo:
     $titulo = $_POST["titulo"];
     $introduccion = $_POST["introduccion"];
     $noticia = $_POST["noticia"];
     $fuente = $_POST["fuente"];
     $categoria = $_POST["categoria"];
-
-    // Nueva entrada para la URL del video
     $videos_url = $_POST["videos_url"];
 
-    // Manejo de archivos de video
-    $videos = array();
-    $target_file = '';
+    // Resto de tu lógica de inserción aquí
 
-    if (!empty($_FILES['videos']['name'][0])) {
-        // Si se subieron archivos de video
-        $target_dir = "resources/videos/"; // Cambia la ruta según tu estructura de carpetas
+    $consultaNoticia = "INSERT INTO noticia (titulo, introduccion, noticia, fotos, fecha_publicacion, fuente, etiquetas) 
+                 VALUES ('$titulo','$introduccion','$noticia','$target_file',curdate(),'$fuente','$categoria')";
 
-        foreach ($_FILES['videos']['tmp_name'] as $key => $tmp_name) {
-            $video_name = $_FILES['videos']['name'][$key];
-            $target_file = $target_dir . basename($video_name);
-            move_uploaded_file($tmp_name, $target_file);
-            $videos[] = $target_file;
+    $resultadoNoticia = mysqli_query($conn, $consultaNoticia);
+
+    if ($resultadoNoticia) {
+        // Si la inserción en la tabla noticia fue exitosa, ahora insertamos en la tabla multimedia
+        $idNoticia = mysqli_insert_id($conn); // Obtener el ID de la última inserción
+
+        $consultaMultimedia = "INSERT INTO multimedia (id_noticia, videos_url) VALUES ('$idNoticia', '$videos_url')";
+        $resultadoMultimedia = mysqli_query($conn, $consultaMultimedia);
+
+        if ($resultadoMultimedia) {
+            echo "<h3 class='ok'>Registro guardado correctamente</h3>";
+        } else {
+            echo "<h3 class='Bad'>Error al insertar en la tabla multimedia</h3>";
         }
-    }
-
-    // Consulta SQL para insertar datos, incluyendo la URL del video
-    $consulta = "INSERT INTO noticia (titulo, introduccion, noticia, fotos, fecha_publicacion, fuente, etiquetas, videos_url) 
-                 VALUES ('$titulo','$introduccion','$noticia','$target_file',curdate(),'$fuente','$categoria','$videos_url')";
-
-    $resultado = mysqli_query($conn, $consulta);
-
-    if ($resultado) {
-        ?>
-        <h3 class="ok">Registro guardado correctamente</h3>
-        <?php
     } else {
-        ?>
-        <h3 class="Bad">Error</h3>
-        <?php
+        echo "<h3 class='Bad'>Error al insertar en la tabla noticia</h3>";
     }
 }
+
+// Reactivar desencadenadores
+mysqli_query($conn, "SET foreign_key_checks = 1;");
 
 ?>
